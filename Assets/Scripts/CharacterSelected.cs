424fabf8f45ca6ac4f971eadbbfc47a94f9af8d2
Stephen Mangum <stephenmangum01@gmail.com>
2025-12-09 13:32:38 -0600

    Q.O.L Improvements + Info Panel for Characters in Development

---
+++ Assets/Scripts/CharacterSelected.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelected: MonoBehaviour
+{
+    public string characterName;             // e.g. "Bits", "Thief", etc.
+    public Sprite normalSprite;              // black sprite
+    public Sprite selectedSprite;            // green sprite
+
+    private Image buttonImage;
+
+    void Start()
+    {
+        buttonImage = GetComponent<Image>();
+        UpdateVisualState();
+    }
+
+    public void OnButtonClicked()
+    {
+        PlayerPrefs.SetString("SelectedCharacter", characterName);
+        PlayerPrefs.Save();
+
+        // Refresh ALL buttons in the scene
+        CharacterSelected[] allButtons = FindObjectsOfType<CharacterSelected>();
+        foreach (var btn in allButtons)
+            btn.UpdateVisualState();
+    }
+
+    public void UpdateVisualState()
+    {
+        string selected = PlayerPrefs.GetString("SelectedCharacter", "Bits");
+
+        if (selected == characterName)
+            buttonImage.sprite = selectedSprite;
+        else
+            buttonImage.sprite = normalSprite;
+    }
+}
